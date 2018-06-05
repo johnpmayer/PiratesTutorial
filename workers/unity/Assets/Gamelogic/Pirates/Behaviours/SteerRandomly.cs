78442eb75364985495bde08172a82ccb5936642f
John Mayer <dev.mayer.john@dev.bwater.com>
2018-06-04 21:37:37 -0400

    Add random steering behavior to pirates

---
+++ workers/unity/Assets/Gamelogic/Pirates/Behaviours/SteerRandomly.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using Improbable.Unity;
+using Improbable.Unity.Visualizer;
+using Improbable.Ship;
+
+namespace Assets.Gamelogic.Pirates.Behaviours
+{
+	// this only runs server-side
+	[WorkerType(WorkerPlatform.UnityWorker)]
+	public class SteerRandomly : MonoBehaviour {
+
+		// only enabled if the worker is allowed to have this component...
+		[Require]
+		// ...specifically, write access to the entity's ship controls component
+		private ShipControls.Writer ShipControlsWriter;
+
+		private void RandomizeSteering()
+		{
+			ShipControlsWriter.Send (new ShipControls.Update ()
+				.SetTargetSpeed (Random.value)
+				.SetTargetSteering ((Random.value * 30.0f) - 15.0f));
+		}
+
+		private void OnEnable()
+		{
+			InvokeRepeating ("RandomizeSteering", 0, 5.0f);
+		}
+
+		private void OnDisable()
+		{
+			CancelInvoke ("RandomizeSteering");
+		}
+
+	}
+}
+
